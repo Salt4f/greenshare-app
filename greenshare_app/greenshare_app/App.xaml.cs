b0d0a628d57dafc81790250d4b231d14889b7126
Guillem <gcasassassala@gmail.com>
2021-10-17 19:18:30 +0200

    borderless entries for both platforms, added fonts and restructured project

--- greenshare_app/greenshare_app/App.xaml.cs
+++ greenshare_app/greenshare_app/App.xaml.cs
@@ -1,5 +1,11 @@
 ﻿using Xamarin.Forms;
 
+[assembly: ExportFont("Montserrat-Bold.ttf", Alias = "Montserrat-Bold")]
+[assembly: ExportFont("Montserrat-Medium.ttf", Alias = "Montserrat-Medium")]
+[assembly: ExportFont("Montserrat-Regular.ttf", Alias = "Montserrat-Regular")]
+[assembly: ExportFont("Montserrat-SemiBold.ttf", Alias = "Montserrat-SemiBold")]
+[assembly: ExportFont("UIFontIcons.ttf", Alias = "FontIcons")]
+
 namespace greenshare_app
 {
     public partial class App : Application
