68161bf68893d8841b9e227fd5dcc8d515d45b6b
Salt4f <47352634+Salt4f@users.noreply.github.com>
2021-10-20 21:30:09 +0200

    Added Utils.Validation class

---
+++ greenshare_app/greenshare_app/Utils/Validation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace greenshare_app.Utils
+{
+    public class Validation
+    {
+        private readonly static Regex emailRegex = new Regex("");
+
+        public static bool ValidateEmail(string email)
+        {
+            return false;
+        }
+
+    }
+}
