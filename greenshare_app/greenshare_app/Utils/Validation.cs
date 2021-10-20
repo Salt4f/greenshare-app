69ad579d38fdce00a8822b21e69cb710c092e1fd
pere.masip <pere.masip@est.fib.upc.edu>
2021-10-20 22:31:40 +0200

    Email validation ready

--- greenshare_app/greenshare_app/Utils/Validation.cs
+++ greenshare_app/greenshare_app/Utils/Validation.cs
@@ -7,11 +7,15 @@ namespace greenshare_app.Utils
 {
     public class Validation
     {
-        private readonly static Regex emailRegex = new Regex("");
+        private readonly static Regex emailRegex = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
 
         public static bool ValidateEmail(string email)
         {
-            return false;
+            if (string.IsNullOrEmpty(email)) return false;
+         
+            Match match = emailRegex.Match(email);
+
+            return match.Success;
         }
 
     }
