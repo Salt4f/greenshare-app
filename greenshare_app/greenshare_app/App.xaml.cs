97ec47080fe50d5223a422f495a154afa179410b
gcasassas <78558771+gcasassas@users.noreply.github.com>
2021-10-25 19:46:45 +0200

    added icons

--- greenshare_app/greenshare_app/App.xaml.cs
+++ greenshare_app/greenshare_app/App.xaml.cs
@@ -1,12 +1,6 @@
 ﻿using Xamarin.Forms;
 using System;
 
-[assembly: ExportFont("Montserrat-Bold.ttf", Alias = "Montserrat-Bold")]
-[assembly: ExportFont("Montserrat-Medium.ttf", Alias = "Montserrat-Medium")]
-[assembly: ExportFont("Montserrat-Regular.ttf", Alias = "Montserrat-Regular")]
-[assembly: ExportFont("Montserrat-SemiBold.ttf", Alias = "Montserrat-SemiBold")]
-[assembly: ExportFont("UIFontIcons.ttf", Alias = "FontIcons")]
-
 namespace greenshare_app
 {
     public partial class App : Application
--- greenshare_app/greenshare_app/AssemblyInfo.cs
+++ greenshare_app/greenshare_app/AssemblyInfo.cs
@@ -1,3 +1,12 @@
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
+[assembly: ExportFont("Montserrat-Bold.ttf", Alias = "Montserrat-Bold")]
+[assembly: ExportFont("Montserrat-Medium.ttf", Alias = "Montserrat-Medium")]
+[assembly: ExportFont("Montserrat-Regular.ttf", Alias = "Montserrat-Regular")]
+[assembly: ExportFont("Montserrat-SemiBold.ttf", Alias = "Montserrat-SemiBold")]
+[assembly: ExportFont("fa-brands-400.ttf", Alias = "FAB")]
+[assembly: ExportFont("fa-regular-400.ttf", Alias = "FAR")]
+[assembly: ExportFont("fa-solid-900.ttf", Alias = "FAS")]
