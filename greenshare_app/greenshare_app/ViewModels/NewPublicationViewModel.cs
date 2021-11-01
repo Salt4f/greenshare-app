0668d18369d47da8b7fcbe1eb2e8f46ee00f798a
marc10010 <75836335+marc10010@users.noreply.github.com>
2021-11-01 19:11:29 +0100

    Primera implementacio crear publicacions

---
+++ greenshare_app/greenshare_app/ViewModels/NewPublicationViewModel.cs
@@ -0,0 +1,49 @@
+﻿using greenshare_app.Utils;
+using greenshare_app.Views;
+using MvvmHelpers;
+using MvvmHelpers.Commands;
+using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Command = MvvmHelpers.Commands.Command;
+
+namespace greenshare_app.ViewModels
+{
+    class NewPublicationViewModel : BaseViewModel
+    {
+        public NewPublicationViewModel(INavigation navigation, Page view)
+        {
+            Title = "New Publication";
+            //Options = Array.Empty;
+
+            this.navigation = navigation;
+            this.view = view;
+        }
+
+        private INavigation navigation;
+        private Page view;
+
+        private string productName;
+        private string description;
+        private string category;
+        //private Array Options;
+
+        public string ProductName
+        {
+            get => productName;
+            set => SetProperty(ref productName, value);
+        }
+        public string Description
+        {
+            get => description;
+            set => SetProperty(ref description, value);
+        }
+        public string Category
+        {
+            get => category;
+            set => SetProperty(ref category, value);
+        }
+
+
+    }
+}
---
+++ greenshare_app/greenshare_app/Views/MainViewPages/NewPublication.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace greenshare_app.Views.MainViewPages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NewPublication : ContentPage
+    {
+        public NewPublication()
+        {
+            InitializeComponent();
+            PickerOption.Items.Add("Request");
+            PickerOption.Items.Add("Offer");
+            PickerCategory.Items.Add("Home");
+            PickerCategory.Items.Add("Videogames");
+            PickerCategory.Items.Add("Fashion");
+            PickerCategory.Items.Add("IT");
+            PickerCategory.Items.Add("Pets");
+            PickerCategory.Items.Add("Sport");
+
+        }
+    }
+}
