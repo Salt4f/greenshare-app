fc90b6ce17567e02b14bb4db7c2fdd64ff465c82
marc10010 <75836335+marc10010@users.noreply.github.com>
2021-11-03 16:33:03 +0100

    adding images

--- greenshare_app/greenshare_app/Views/MainViewPages/NewPublication.xaml.cs
+++ greenshare_app/greenshare_app/Views/MainViewPages/NewPublication.xaml.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -24,6 +26,37 @@ namespace greenshare_app.Views.MainViewPages
             PickerCategory.Items.Add("Pets");
             PickerCategory.Items.Add("Sport");
 
+        }
+        async void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            //! added using Plugin.Media;
+            await CrossMedia.Current.Initialize();
+
+            //// if you want to take a picture use this
+            // if(!CrossMedia.Current.IsTakePhotoSupported || !CrossMedia.Current.IsCameraAvailable)
+            /// if you want to select from the gallery use this
+            if (!CrossMedia.Current.IsPickPhotoSupported)
+            {
+                await DisplayAlert("Not supported", "Your device does not currently support this functionality", "Ok");
+                return;
+            }
+
+            //! added using Plugin.Media.Abstractions;
+            // if you want to take a picture use StoreCameraMediaOptions instead of PickMediaOptions
+            var mediaOptions = new PickMediaOptions()
+            {
+                PhotoSize = PhotoSize.Small
+            };
+            // if you want to take a picture use TakePhotoAsync instead of PickPhotoAsync
+            var selectedImageFile = await CrossMedia.Current.PickPhotoAsync(mediaOptions);
+
+            if (selectedImage == null)
+            {
+                await DisplayAlert("Error", "Could not get the image, please try again.", "Ok");
+                return;
+            }
+
+            selectedImage.Source = ImageSource.FromStream(() => selectedImageFile.GetStream());
         }
     }
 }
