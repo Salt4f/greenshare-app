28e6263ef940b45305354037f3350f9dd77f0d46
pere.masip <pere.masip@estudiantat.upc.edu>
2021-11-15 19:50:30 +0100

    Get util created

---
+++ greenshare_app/greenshare_app/Utils/Get.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace greenshare_app.Utils
+{
+    class Get
+    {
+        private static Get instance;
+        private Get()
+        {
+            httpClient = new HttpClient();
+        }
+        public static Get Instance()
+        {
+            if (instance is null) instance = new Get();
+            return instance;
+        }
+
+        private HttpClient httpClient;
+
+    }
+}
