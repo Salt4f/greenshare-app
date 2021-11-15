f5f4d02096f0a5858bcdb97aaa363250f4f5dcc9
pere.masip <pere.masip@estudiantat.upc.edu>
2021-11-15 20:34:20 +0100

    NullLocationException Comments

--- greenshare_app/greenshare_app/Exceptions/NullLocationException.cs
+++ greenshare_app/greenshare_app/Exceptions/NullLocationException.cs
@@ -6,14 +6,14 @@ namespace greenshare_app.Exceptions
     public class NullLocationException : Exception
     {
         /// <summary>
-        /// Initializes a new instance of the <see cref="T:MyException"/> class
+        /// Initializes a new instance of the <see cref="T:NullLocationException"/> class
         /// </summary>
         public NullLocationException()
         {
         }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="T:MyException"/> class
+        /// Initializes a new instance of the <see cref="T:NullLocationException"/> class
         /// </summary>
         /// <param name="message">A <see cref="T:System.String"/> that describes the exception. </param>
         public NullLocationException(string message) : base(message)
@@ -21,7 +21,7 @@ namespace greenshare_app.Exceptions
         }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="T:MyException"/> class
+        /// Initializes a new instance of the <see cref="T:NullLocationException"/> class
         /// </summary>
         /// <param name="message">A <see cref="T:System.String"/> that describes the exception. </param>
         /// <param name="inner">The exception that is the cause of the current exception. </param>
@@ -30,7 +30,7 @@ namespace greenshare_app.Exceptions
         }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="T:MyException"/> class
+        /// Initializes a new instance of the <see cref="T:NullLocationException"/> class
         /// </summary>
         /// <param name="context">The contextual information about the source or destination.</param>
         /// <param name="info">The object that holds the serialized object data.</param>
