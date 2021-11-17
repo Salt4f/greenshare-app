353c9faaa9db7b6cd7862bf163dd49595ec6c717
ThePlanso <91215812+ThePlanso2@users.noreply.github.com>
2021-11-17 20:00:47 +0100

    Fixed some little errors

--- greenshare_app/greenshare_app/Views/MainViewPages/ProfilePage.xaml.cs
+++ greenshare_app/greenshare_app/Views/MainViewPages/ProfilePage.xaml.cs
@@ -2,8 +2,6 @@
 using greenshare_app.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
-using Plugin.Media;
-using Plugin.Media.Abstractions;
 
 namespace greenshare_app.Views.MainViewPages
 {
@@ -14,38 +12,6 @@ namespace greenshare_app.Views.MainViewPages
         {
             InitializeComponent();
             BindingContext = new ViewModels.ProfilePageViewModel(Navigation, this);
-        }
-
-        async void Handle_Clicked(object sender, System.EventArgs e)
-        {
-            //! added using Plugin.Media;
-            await CrossMedia.Current.Initialize();
-
-            //// if you want to take a picture use this
-            // if(!CrossMedia.Current.IsTakePhotoSupported || !CrossMedia.Current.IsCameraAvailable)
-            /// if you want to select from the gallery use this
-            if (!CrossMedia.Current.IsPickPhotoSupported)
-            {
-                await DisplayAlert("Not supported", "Your device does not currently support this functionality", "Ok");
-                return;
-            }
-
-            //! added using Plugin.Media.Abstractions;
-            // if you want to take a picture use StoreCameraMediaOptions instead of PickMediaOptions
-            var mediaOptions = new PickMediaOptions()
-            {
-                PhotoSize = PhotoSize.Small
-            };
-            // if you want to take a picture use TakePhotoAsync instead of PickPhotoAsync
-            var selectedImageFile = await CrossMedia.Current.PickPhotoAsync(mediaOptions);
-
-            if (selectedImage == null)
-            {
-                await DisplayAlert("Error", "Could not get the image, please try again.", "Ok");
-                return;
-            }
-
-            selectedImage.Source = ImageSource.FromStream(() => selectedImageFile.GetStream());
         }
 
     }
