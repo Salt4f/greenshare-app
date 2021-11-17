b6683ff5f1ffaa379b11b286d8a9c3539ed3cac3
ThePlanso <91215812+ThePlanso2@users.noreply.github.com>
2021-11-17 19:50:03 +0100

    Solucionado el problema del zip

--- greenshare_app/greenshare_app/ViewModels/ProfilePageViewModel.cs
+++ greenshare_app/greenshare_app/ViewModels/ProfilePageViewModel.cs
@@ -1,12 +1,29 @@
 ﻿using MvvmHelpers;
+using greenshare_app.Utils;
+using greenshare_app.Views;
+using MvvmHelpers.Commands;
+using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Command = MvvmHelpers.Commands.Command;
 
 namespace greenshare_app.ViewModels
 {
     public class ProfilePageViewModel : BaseViewModel
     {
-        public ProfilePageViewModel()
+        public ProfilePageViewModel(INavigation navigation, Page view)
         {
             Title = "Perfil";
+            this.navigation = navigation;
+            this.view = view;
+        }
+
+        public Command UserInfoCommand => new Command(OnUserInfoClicked);
+        private INavigation navigation;
+        private Page view;
+        private async void OnUserInfoClicked()
+        {
+            await navigation.PushModalAsync(new Views.MainViewPages.RequestsPage());
         }
     }
 }
--- greenshare_app/greenshare_app/Views/MainViewPages/ProfilePage.xaml.cs
+++ greenshare_app/greenshare_app/Views/MainViewPages/ProfilePage.xaml.cs
@@ -2,6 +2,8 @@
 using greenshare_app.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
 
 namespace greenshare_app.Views.MainViewPages
 {
@@ -11,7 +13,39 @@ namespace greenshare_app.Views.MainViewPages
         public ProfilePage()
         {
             InitializeComponent();
-            BindingContext = new ProfilePageViewModel();
+            BindingContext = new ViewModels.ProfilePageViewModel(Navigation, this);
+        }
+
+        async void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            //! added using Plugin.Media;
+            await CrossMedia.Current.Initialize();
+
+            //// if you want to take a picture use this
+            // if(!CrossMedia.Current.IsTakePhotoSupported || !CrossMedia.Current.IsCameraAvailable)
+            /// if you want to select from the gallery use this
+            if (!CrossMedia.Current.IsPickPhotoSupported)
+            {
+                await DisplayAlert("Not supported", "Your device does not currently support this functionality", "Ok");
+                return;
+            }
+
+            //! added using Plugin.Media.Abstractions;
+            // if you want to take a picture use StoreCameraMediaOptions instead of PickMediaOptions
+            var mediaOptions = new PickMediaOptions()
+            {
+                PhotoSize = PhotoSize.Small
+            };
+            // if you want to take a picture use TakePhotoAsync instead of PickPhotoAsync
+            var selectedImageFile = await CrossMedia.Current.PickPhotoAsync(mediaOptions);
+
+            if (selectedImage == null)
+            {
+                await DisplayAlert("Error", "Could not get the image, please try again.", "Ok");
+                return;
+            }
+
+            selectedImage.Source = ImageSource.FromStream(() => selectedImageFile.GetStream());
         }
 
     }
