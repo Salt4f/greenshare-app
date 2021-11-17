7f8dde8dc6c319829b2a46f1df319ff30dbff7f9
marc10010 <75836335+marc10010@users.noreply.github.com>
2021-11-17 20:27:10 +0100

    borderlessPicker

---
+++ greenshare_app/greenshare_app.Android/Renderers/BorderlessPickerRenderer.cs
@@ -0,0 +1,31 @@
+﻿using Android.Views;
+using greenshare_app.Controls;
+using greenshare_app.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using Application = Android.App.Application;
+
+[assembly: ExportRenderer(typeof(BorderlessPicker), typeof(BorderlessPickerRenderer))]
+namespace greenshare_app.Droid
+{
+    public class BorderlessPickerRenderer : PickerRenderer
+    {
+        public static void Init() { }
+        protected override void OnElementChanged(ElementChangedEventArgs<Picker> e)
+        {
+            base.OnElementChanged(e);
+            if (e.OldElement == null)
+            {
+                Control.Background = null;
+
+                var layoutParams = new MarginLayoutParams(Control.LayoutParameters);
+                layoutParams.SetMargins(0, 0, 0, 0);
+                LayoutParameters = layoutParams;
+                Control.LayoutParameters = layoutParams;
+                Control.SetPadding(0, 0, 0, 0);
+                Control.Gravity = GravityFlags.CenterVertical;
+                SetPadding(0, 0, 0, 0);
+            }
+        }
+    }
+}
---
+++ greenshare_app/greenshare_app.iOS/Renderers/BorderlessPickerRenderer.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+
+[assembly: ExportRenderer(typeof(greenshare_app.Controls.BorderlessPicker), typeof(greenshare_app.iOS.BorderlessPickerRenderer))]
+namespace greenshare_app.iOS
+{
+    public class BorderlessPickerRenderer : PickerRenderer
+    {
+        public static void Init() { }
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+
+            Control.Layer.BorderWidth = 0;
+            Control.BorderStyle = UITextBorderStyle.None;
+        }
+    }
+}
