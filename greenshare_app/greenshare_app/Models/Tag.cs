faca46573177879eb54ba6bcf53071bb72a377f1
Sergio Camps <47352634+Salt4f@users.noreply.github.com>
2021-11-17 19:19:53 +0100

    Update Tag.cs

--- greenshare_app/greenshare_app/Models/Tag.cs
+++ greenshare_app/greenshare_app/Models/Tag.cs
@@ -1,12 +1,14 @@
-﻿using Xamarin.Forms;
+﻿using Newtonsoft.Json;
+using Xamarin.Forms;
 
 namespace greenshare_app.Models
 {
     public class Tag
     {
+        [JsonProperty(PropertyName = "color")]
         public Color Color { get; set; }
+        [JsonProperty(PropertyName = "name")]
         public string Name { get; set; }
-
       
     }
 }
