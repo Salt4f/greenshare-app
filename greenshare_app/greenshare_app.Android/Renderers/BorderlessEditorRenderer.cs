4bff9f4b455c85e5fbc75c96a6b60336b5b35e6d
marc10010 <75836335+marc10010@users.noreply.github.com>
2021-11-24 19:46:45 +0100

    Borderless Entry

---
+++ greenshare_app/greenshare_app.Android/Renderers/BorderlessEditorRenderer.cs
@@ -0,0 +1,31 @@
+﻿using Android.Graphics.Drawables;
+using Android.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using Application = Android.App.Application;
+
+[assembly: ExportRenderer(typeof(greenshare_app.Controls.BorderlessEditor), typeof(greenshare_app.Droid.BorderlessEditorRenderer))]
+
+namespace greenshare_app.Droid
+{
+    [System.Obsolete]
+    public class BorderlessEditorRenderer : EditorRenderer
+    {
+        public static void Init() { }
+        protected override void OnElementChanged(ElementChangedEventArgs<Editor> e)
+        {
+            base.OnElementChanged(e);
+            if (e.OldElement == null)
+            {
+                Control.Background = null;
+
+                var layoutParams = new MarginLayoutParams(Control.LayoutParameters);
+                layoutParams.SetMargins(0, 0, 0, 0);
+                LayoutParameters = layoutParams;
+                Control.LayoutParameters = layoutParams;
+                Control.SetPadding(0, 0, 0, 0);
+                SetPadding(0, 0, 0, 0);
+            }
+        }
+    }
+}
