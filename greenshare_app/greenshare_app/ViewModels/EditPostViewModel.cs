12eb8f3e61562821c2be22abfab488d6055b545c
pere.masip <pere.masip@estudiantat.upc.edu>
2021-11-28 15:03:25 +0100

    EditPostViewModel created

---
+++ greenshare_app/greenshare_app/ViewModels/EditPostViewModel.cs
@@ -0,0 +1,11 @@
+﻿using MvvmHelpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace greenshare_app.ViewModels
+{
+    public class EditPostViewModel : BaseViewModel
+    {
+    }
+}
