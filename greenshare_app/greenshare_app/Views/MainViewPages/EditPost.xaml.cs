2a8cb7b2d1f55eaa90c007efb283735c325e63e3
marc10010 <75836335+marc10010@users.noreply.github.com>
2021-12-06 21:24:57 +0100

    edit post like newPost

--- greenshare_app/greenshare_app/Views/MainViewPages/EditPost.xaml.cs
+++ greenshare_app/greenshare_app/Views/MainViewPages/EditPost.xaml.cs
@@ -3,18 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using greenshare_app.ViewModels;
+using greenshare_app.Models;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace greenshare_app.Views.MainViewPages
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class EditPage : ContentPage
+    public partial class EditPost : ContentPage
     {
-        public EditPage()
+        public EditPost(Offer offer)
         {
             InitializeComponent();
+            BindingContext = new EditPostViewModel(Navigation, this, offer);
+        }
+
+        private void selectedImage_Clicked(object sender, EventArgs e)
+        {
+
         }
     }
 }
