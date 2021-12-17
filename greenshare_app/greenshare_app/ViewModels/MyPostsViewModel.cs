38829b5564272b8152c6d4b55185a0ef339c2635
pere.masip <pere.masip@estudiantat.upc.edu>
2021-12-17 18:10:15 +0100

    New Pages ready for work

---
+++ greenshare_app/greenshare_app/ViewModels/MyPostsViewModel.cs
@@ -0,0 +1,21 @@
+﻿using greenshare_app.Models;
+using MvvmHelpers;
+using MvvmHelpers.Commands;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using greenshare_app.Utils;
+using System.Text;
+
+namespace greenshare_app.ViewModels
+{
+    internal class MyPostsViewModel : BaseViewModel
+    {
+        public MyPostsViewModel(INavigation navigation, Page view)
+        {
+            Title = "My Posts";
+        }
+    }
+}
