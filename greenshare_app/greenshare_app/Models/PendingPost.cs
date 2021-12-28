a8ad8222185b5c410464d216d1a571aba6ff1a56
pere.masip <pere.masip@estudiantat.upc.edu>
2021-12-28 12:16:20 +0100

    Added Simple PendingOffersViewModel
    Added property AcceptButtonsVisible to PendingPost model

--- greenshare_app/greenshare_app/Models/PendingPost.cs
+++ greenshare_app/greenshare_app/Models/PendingPost.cs
@@ -9,6 +9,7 @@ namespace greenshare_app.Models
         public String UserNickName { get; set; }
         public String UserId { get; set; }
         public String PostName { get; set; }
+        public bool AcceptButtonsVisible { get; set; }//En caso de que sea una pending offer creada por nosotros y no esté completada será True. En los demás, False. Lo calculamos nosotros
         public int PostId { get; set; }//Id del post al que aplico
         public int OwnPostId { get; set; }//Id de mi propio post
 
