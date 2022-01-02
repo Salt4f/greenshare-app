88f9966cd551a0ed92cedd28f4e70143d061cf96
gcasassas <78558771+gcasassas@users.noreply.github.com>
2022-01-02 18:57:53 +0100

    Lists rework

--- greenshare_app/greenshare_app/Models/PendingPostInteraction.cs
+++ greenshare_app/greenshare_app/Models/PendingPostInteraction.cs
@@ -11,6 +11,10 @@ namespace greenshare_app.Models
         public string PostType { get; set; }
         public int PostId { get; set; }
         public string PostName { get; set; }
+        public string InteractionText { get; set; }
+        //sera el text que indicarà que està succeint en aquella interacció
+        //ex: en guillem t'està oferint a la teva solicitud d'una bicicleta
+        //ex: en guillem t'està sol·licitant el teu ordinador
         public int OwnPostId { get; set; }
 
     }
