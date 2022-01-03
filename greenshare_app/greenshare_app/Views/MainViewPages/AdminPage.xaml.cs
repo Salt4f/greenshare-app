31a7901d25d5e79b2e43841c642ce3724895eef2
ThePlanso <91215812+ThePlanso2@users.noreply.github.com>
2022-01-03 13:25:29 +0100

    Primera implementació vista Admin

---
+++ greenshare_app/greenshare_app/Views/MainViewPages/AdminPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace greenshare_app.Views.MainViewPages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AdminPage : ContentPage
+    {
+        public ObservableCollection<string> Items { get; set; }
+        public string ReportPostOrUser { get; set; }
+
+        public AdminPage()
+        {
+            InitializeComponent();
+
+            Items = new ObservableCollection<string>
+            {
+                "Item 1",
+                "Item 2",
+                "Item 3",
+                "Item 4",
+                "Item 5"
+            };
+            ReportPostOrUser = "Post";
+
+            MyListView.ItemsSource = Items;
+        }
+
+        async void Handle_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            if (e.Item == null)
+                return;
+
+            await DisplayAlert("Item Tapped", "An item was tapped.", "OK");
+
+            //Deselect Item
+            ((ListView)sender).SelectedItem = null;
+        }
+    }
+}
