4428660c57e128583924cc7d7c734d1f80550ed0
marc10010 <75836335+marc10010@users.noreply.github.com>
2022-01-10 00:07:54 +0100

    vista quiz a falta de añadir preguntas del servicio

---
+++ greenshare_app/greenshare_app/Views/QuizView.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using greenshare_app.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace greenshare_app.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class QuizView : ContentPage
+    {
+        public QuizView()
+        {
+            InitializeComponent();
+            BindingContext = new QuizPagePostViewModel(Navigation, this);
+        }
+    }
+}
