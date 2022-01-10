4d48105a80bf271d63bbe25b47daa756aad9cc5b
ThePlanso <91215812+ThePlanso2@users.noreply.github.com>
2022-01-10 17:05:15 +0100

    Implemetació vista rewards

---
+++ greenshare_app/greenshare_app/Models/Reward.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace greenshare_app.Models
+{
+    class Reward
+    {
+        public int Id { get; set; }
+        public string SponsorName { get; set; }
+        public int GreenCoins { get; set; }
+        public string GreenCoinsText { get; set; }
+        public string Description { get; set; }
+    }
+}
