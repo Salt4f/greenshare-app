52fb5f1858569b8a00a342a04a716583a2ad589e
pmasipr <pere.masip@estudiantat.upc.edu>
2022-01-13 21:12:43 +0100

    added logos

--- greenshare_app/greenshare_app.Android/MainActivity.cs
+++ greenshare_app/greenshare_app.Android/MainActivity.cs
@@ -8,7 +8,7 @@ using System.Drawing;
 
 namespace greenshare_app.Droid
 {
-    [Activity(Label = "greenshare_app", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+    [Activity(Label = "greenshare_app", Icon = "@mipmap/logo", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
